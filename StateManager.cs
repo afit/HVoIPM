5e8f3f3ac5abeef5d9ad0083c1c4bf166f2b2872
Aidan Fitzpatrick <aidan@reincubate.com>
2005-12-30 12:15:39 +0000

    Change -> PropertyChange Added DMC support Added support for multiple behaviours per propertyChange

--- StateManager.cs
+++ StateManager.cs
@@ -10,7 +10,6 @@ using System.Xml;
 using LothianProductions.Data;
 using LothianProductions.Util;
 using LothianProductions.Util.Settings;
-using LothianProductions.VoIP.Behaviour;
 using LothianProductions.VoIP.Monitor;
 using LothianProductions.VoIP.Monitor.Impl;
 using LothianProductions.VoIP.State;
@@ -22,14 +21,14 @@ namespace LothianProductions.VoIP {
 		protected static readonly StateManager mInstance = new StateManager();
 
         protected Dictionary<Call, CallRecord> mCalls = new Dictionary<Call, CallRecord>();
-        protected Dictionary<Object, Dictionary<String, StateChangeBehaviour>> mStateProperties = new Dictionary<Object, Dictionary<String, StateChangeBehaviour>>();
+        protected Dictionary<Object, Dictionary<String, PropertyBehaviour>> mStateProperties = new Dictionary<Object, Dictionary<String, PropertyBehaviour>>();
 		
 		public static StateManager Instance() {
 			return mInstance;
 		}
 		
 		public event StateUpdateHandler StateUpdate;
-		protected Dictionary<IDeviceMonitor, Thread> mDeviceStateMonitors = new Dictionary<IDeviceMonitor, Thread>();
+		protected Dictionary<DeviceMonitorControl, Thread> mDeviceMonitorControls = new Dictionary<DeviceMonitorControl, Thread>();
     
 		protected StateManager() {
 			// Initialize device monitors, have to give each its own thread.
@@ -37,12 +36,12 @@ namespace LothianProductions.VoIP {
 		}
 
 		public void ReloadDeviceStateMonitors() {
-			lock (mDeviceStateMonitors) {
+			lock (mDeviceMonitorControls) {
 				// Stop currently running threads.
-				foreach( IDeviceMonitor monitor in mDeviceStateMonitors.Keys )
-					mDeviceStateMonitors[ monitor ].Abort();
+				foreach( DeviceMonitorControl control in mDeviceMonitorControls.Keys )
+					mDeviceMonitorControls[ control ].Abort();
 					
-				mDeviceStateMonitors.Clear();
+				mDeviceMonitorControls.Clear();
 
 				NameValueCollection config = (NameValueCollection) ConfigurationManager.GetSection( "hvoipm/deviceStateMonitors" );
 
@@ -60,42 +59,46 @@ namespace LothianProductions.VoIP {
 						new String[] { key }
 					);
 
-					Thread thread = new Thread( new ThreadStart( monitor.Run ) );
+					DeviceMonitorControl control = new DeviceMonitorControl( monitor );
+
+					Thread thread = new Thread( new ThreadStart( control.Run ) );
 					
-					mDeviceStateMonitors.Add( monitor, thread );
+					mDeviceMonitorControls.Add( control, thread );
 					thread.Start();
                 }
                 
-                Logger.Instance().Log( "Loaded " + mDeviceStateMonitors.Count + " device state monitor(s)" );
+                Logger.Instance().Log( "Loaded " + mDeviceMonitorControls.Count + " device state monitor(s)" );
 			}
 		}
 		
-		public ICollection<IDeviceMonitor> DeviceStateMonitors {
-			get{ return mDeviceStateMonitors.Keys; }
+		public ICollection<DeviceMonitorControl> DeviceMonitorControls {
+			get{ return mDeviceMonitorControls.Keys; }
 		}
 
-		public void DeviceStateUpdated(	IDeviceMonitor monitor, IList<DeviceChange> deviceChanges,
-										IList<LineChange> lineChanges, IList<CallChange> callChanges ) {
+		public void DeviceStateUpdated(	IDeviceMonitor monitor, IList<DevicePropertyChange> deviceChanges,
+										IList<LinePropertyChange> lineChanges, IList<CallPropertyChange> callChanges ) {
 			// FIXME this is very inefficient
-			List<Change> changes = new List<Change>();
-			foreach( Change change in deviceChanges )
+			List<PropertyChange> changes = new List<PropertyChange>();
+			foreach( PropertyChange change in deviceChanges )
 				changes.Add( change );
-			foreach( Change change in lineChanges )
+			foreach( PropertyChange change in lineChanges )
 				changes.Add( change );
-			foreach( Change change in callChanges )
+			foreach( PropertyChange change in callChanges )
 				changes.Add( change );
 				
-			foreach( Change change in changes )
-				if( LookupBehaviour( change.Underlying, change.Property ).Log )
+			foreach( PropertyChange change in changes )
+				// Note that we log changing to or from any given logging criteria
+				if(	LookupPropertyChangeBehaviour( change.Underlying, change.Property, change.ChangedTo ).Log ||
+					LookupPropertyChangeBehaviour( change.Underlying, change.Property, change.ChangedFrom ).Log )
 					Logger.Instance().Log( change.Underlying.GetType().Name + " property " + change.Property + " has changed from " + change.ChangedFrom + " to " + change.ChangedTo );
 						
 			// Logging happens here:
-			foreach( CallChange change in callChanges ) {
+			foreach( CallPropertyChange change in callChanges ) {
 				if( change.Property == DeviceMonitor.PROPERTY_CALL_ACTIVITY ) {
                     if ( change.ChangedTo == Activity.Connected.ToString() ) {
                         CallRecord call = new CallRecord( monitor.GetDeviceState(), GetLine( change.Call ), change.Call, DateTime.Now, new DateTime() );
 						
-						// FIXME add sanity check in case somehow call is already there
+						// Sanity check in case somehow call is already there
 						if ( mCalls.ContainsKey( change.Call ) ) {
 							mCalls.Remove( change.Call );
 							Logger.Instance().Log("Call #" + change.Call.Name + " had to be removed from the call list - did the previous call fail?");
@@ -123,85 +126,123 @@ namespace LothianProductions.VoIP {
 				StateUpdate( monitor, new StateUpdateEventArgs( deviceChanges, lineChanges, callChanges ) );
 		}
 		
-		public StateChangeBehaviour LookupBehaviour( Object state, String property ) {		
+		public PropertyChangeBehaviour LookupPropertyChangeBehaviour( Object state, String property, String criteria ) {
+			if( LookupPropertyBehaviour( state, property ).PropertyChangeBehaviours.ContainsKey( criteria ) )
+				return LookupPropertyBehaviour( state, property ).PropertyChangeBehaviours[ criteria ];
+			if( LookupPropertyBehaviour( state, property ).PropertyChangeBehaviours.ContainsKey( "" ) )
+				return LookupPropertyBehaviour( state, property ).PropertyChangeBehaviours[ "" ];
+				
+			throw new ConfigurationErrorsException( "Could not find behaviours suitable for criteria \"" + criteria + "\" for property \"" + property + "\" in application configuration." );
+		}
+		
+		public PropertyBehaviour LookupPropertyBehaviour( Object state, String property ) {		
 			// Behaviour not set yet.
 			if( ! mStateProperties.ContainsKey( state ) )
-				mStateProperties.Add( state, new Dictionary<String, StateChangeBehaviour>() );
+				mStateProperties.Add( state, new Dictionary<String, PropertyBehaviour>() );
 			
-			Dictionary<String, StateChangeBehaviour> propertyBehaviours = mStateProperties[ state ];
+			Dictionary<String, PropertyBehaviour> propertyBehaviours = mStateProperties[ state ];
 			
 			if( ! propertyBehaviours.ContainsKey( property ) )
-				propertyBehaviours.Add( property, GetBehaviourFromXml( state.GetType().Name, property ) );
+				propertyBehaviours.Add( property, GetPropertyBehaviourFromXml( state.GetType().Name, property ) );
 			
 			return propertyBehaviours[ property ];	
 		}
 		
-		protected StateChangeBehaviour GetBehaviourFromXml( String stateType, String property ) {
+		protected PropertyBehaviour GetPropertyBehaviourFromXml( String stateType, String property ) {
 			XmlNode node = (XmlNode) ConfigurationManager.GetSection( "hvoipm/behaviours" );
 			
 			if( node == null )
 				throw new ConfigurationErrorsException( "Could not find behaviours section in application configuration." );
 
-			XmlNode behaviour = node.SelectSingleNode( "behaviour[@stateType='" + stateType + "' and @property='" + property + "']" );
-			if( behaviour == null )
+			XmlNode propertyNode = node.SelectSingleNode( "property[@stateType='" + stateType + "' and @property='" + property + "']" );
+			if( propertyNode == null )
 				throw new ConfigurationErrorsException( "Could not find behaviour description for " + stateType + "." + property + "\" in application configuration." );
 
-			return new StateChangeBehaviour(
-				Boolean.Parse( behaviour.Attributes[ "showBubble" ].Value ),
-				behaviour.Attributes[ "bubbleText" ].Value,
-				Boolean.Parse( behaviour.Attributes[ "systemTrayWarning" ].Value ),
-				Boolean.Parse( behaviour.Attributes[ "showApplication" ].Value ),
-				behaviour.Attributes[ "externalProcess" ].Value,
-				( behaviour.Attributes[ "warningCriteria" ] == null ? "" : behaviour.Attributes[ "warningCriteria" ].Value ).Split( ',' ),
-				Boolean.Parse( behaviour.Attributes[ "log" ].Value )
+			Dictionary<String, PropertyChangeBehaviour> changeBehaviours = new Dictionary<String, PropertyChangeBehaviour>();
+			PropertyBehaviour behaviour = new PropertyBehaviour(
+				propertyNode.Attributes[ "label" ].Value,
+				changeBehaviours
 			);
+			
+			foreach( XmlNode behaviourNode in propertyNode.ChildNodes )
+				// Just add a single behaviour entry if it's a catch-all criteria
+				// or if there's only one.
+			    if( behaviourNode.Attributes[ "warningCriteria" ] == null || ! behaviourNode.Attributes[ "warningCriteria" ].Value.Contains( "," ) )
+					changeBehaviours.Add(
+						( behaviourNode.Attributes[ "warningCriteria" ] == null ? "" : behaviourNode.Attributes[ "warningCriteria" ].Value ),
+						new PropertyChangeBehaviour(
+							Boolean.Parse( behaviourNode.Attributes[ "showBubble" ].Value ),
+							behaviourNode.Attributes[ "bubbleText" ].Value,
+							Boolean.Parse( behaviourNode.Attributes[ "systemTrayWarning" ].Value ),
+							Boolean.Parse( behaviourNode.Attributes[ "showApplication" ].Value ),
+							behaviourNode.Attributes[ "externalProcess" ].Value,
+							( behaviourNode.Attributes[ "warningCriteria" ] == null ? "" : behaviourNode.Attributes[ "warningCriteria" ].Value ),
+							Boolean.Parse( behaviourNode.Attributes[ "log" ].Value )
+						)
+					);
+				else
+					foreach( String criteria in behaviourNode.Attributes[ "warningCriteria" ].Value.Split( ',' ) )
+						changeBehaviours.Add(
+							criteria,
+							new PropertyChangeBehaviour(
+								Boolean.Parse( behaviourNode.Attributes[ "showBubble" ].Value ),
+								behaviourNode.Attributes[ "bubbleText" ].Value,
+								Boolean.Parse( behaviourNode.Attributes[ "systemTrayWarning" ].Value ),
+								Boolean.Parse( behaviourNode.Attributes[ "showApplication" ].Value ),
+								behaviourNode.Attributes[ "externalProcess" ].Value,
+								criteria,
+								Boolean.Parse( behaviourNode.Attributes[ "log" ].Value )
+							)
+						);
+
+			return behaviour;
 		}
 		
 		// Helper functions for linking states.
 		public IDeviceMonitor GetMonitor( Device deviceState ) {
-			foreach( IDeviceMonitor monitor in DeviceStateMonitors )
-				if( monitor.GetDeviceState() == deviceState )
-					return monitor;
+			foreach( DeviceMonitorControl control in DeviceMonitorControls )
+				if( control.DeviceMonitor.GetDeviceState() == deviceState )
+					return control.DeviceMonitor;
 			throw new DomainObjectNotFoundException( "Couldn't find a monitor owning the specified device." );
 		}
 
 		public IDeviceMonitor GetMonitor( Line lineState ) {
-			foreach( IDeviceMonitor monitor in DeviceStateMonitors )
-				foreach( Line line in monitor.GetDeviceState().Lines )
+			foreach( DeviceMonitorControl control in DeviceMonitorControls )
+				foreach( Line line in control.DeviceMonitor.GetDeviceState().Lines )
 					if( line == lineState )
-						return monitor;
+						return control.DeviceMonitor;
 			throw new DomainObjectNotFoundException( "Couldn't find a monitor owning the specified line." );
 		}
 
 		public IDeviceMonitor GetMonitor( Call callState ) {
-			foreach( IDeviceMonitor monitor in DeviceStateMonitors )
-				foreach( Line line in monitor.GetDeviceState().Lines )
+			foreach( DeviceMonitorControl control in DeviceMonitorControls )
+				foreach( Line line in control.DeviceMonitor.GetDeviceState().Lines )
 					foreach( Call call in line.Calls )
 						if( call == callState )
-							return monitor;
+							return control.DeviceMonitor;
 			throw new DomainObjectNotFoundException( "Couldn't find a monitor owning the specified call." );
 		}
 		
 		public Device GetDevice( Line lineState ) {
-			foreach( IDeviceMonitor monitor in DeviceStateMonitors )
-				foreach( Line line in monitor.GetDeviceState().Lines )
+			foreach( DeviceMonitorControl control in DeviceMonitorControls )
+				foreach( Line line in control.DeviceMonitor.GetDeviceState().Lines )
 					if( line == lineState )
-						return monitor.GetDeviceState();
+						return control.DeviceMonitor.GetDeviceState();
 			throw new DomainObjectNotFoundException( "Couldn't find a device owning the specified line." );
 		}
 		
 		public Device GetDevice( Call callState ) {
-			foreach( IDeviceMonitor monitor in DeviceStateMonitors )
-				foreach( Line line in monitor.GetDeviceState().Lines )
+			foreach( DeviceMonitorControl control in DeviceMonitorControls )
+				foreach( Line line in control.DeviceMonitor.GetDeviceState().Lines )
 					foreach( Call call in line.Calls )
 						if( call == callState )
-							return monitor.GetDeviceState();
+							return control.DeviceMonitor.GetDeviceState();
 			throw new DomainObjectNotFoundException( "Couldn't find a device owning the specified call." );
 		}
 		
 		public Line GetLine( Call callState ) {
-			foreach( IDeviceMonitor monitor in DeviceStateMonitors )
-				foreach( Line line in monitor.GetDeviceState().Lines )
+			foreach( DeviceMonitorControl control in DeviceMonitorControls )
+				foreach( Line line in control.DeviceMonitor.GetDeviceState().Lines )
 					foreach( Call call in line.Calls )
 						if( call == callState )
 							return line;
