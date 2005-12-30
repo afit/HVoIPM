e65e3cd59022ede5573f5a3c3ccd76632827766c
Aidan Fitzpatrick <aidan@reincubate.com>
2005-12-30 12:09:41 +0000

    Change -> PropertyChange Enum lookups into Xml

--- Monitor/Impl/LinksysPAP2DeviceMonitor.cs
+++ Monitor/Impl/LinksysPAP2DeviceMonitor.cs
@@ -54,14 +54,14 @@ namespace LothianProductions.VoIP.Monitor.Impl {
 					} catch (WebException e) {
 					    if( e.Status == WebExceptionStatus.ProtocolError )
 							if( ( (HttpWebResponse) e.Response ).StatusCode == HttpStatusCode.Unauthorized )
-					            throw new UnauthorizedException( "You are not authorized to request \"" + hostname + "\".", e );
+					            throw new DeviceAccessUnauthorizedException( "Not authorized to request \"" + hostname + "\"; perhaps username and password are incorrect?", e );
 					            
-					    throw new DeviceNotRespondingException( "The device is not responding to status requests:", e );
+					    throw new DeviceNotRespondingException( "The device \"" + hostname + "\" is not responding to status requests", e );
 					}
 					
-					IList<DeviceChange> deviceChanges = new List<DeviceChange>();
-					IList<LineChange> lineChanges = new List<LineChange>();
-					IList<CallChange> callChanges = new List<CallChange>();
+					IList<DevicePropertyChange> deviceChanges = new List<DevicePropertyChange>();
+					IList<LinePropertyChange> lineChanges = new List<LinePropertyChange>();
+					IList<CallPropertyChange> callChanges = new List<CallPropertyChange>();
 				
 					AnalyseLineState( page, mDeviceState.Lines[ 0 ], lineChanges, callChanges );	
 					AnalyseLineState( page, mDeviceState.Lines[ 1 ], lineChanges, callChanges );
@@ -74,26 +74,26 @@ namespace LothianProductions.VoIP.Monitor.Impl {
 			}
         }
         
-        protected virtual void AnalyseLineState( String page, Line lineState, IList<LineChange> lineChanges, IList<CallChange> callChanges ) {
+        protected virtual void AnalyseLineState( String page, Line lineState, IList<LinePropertyChange> lineChanges, IList<CallPropertyChange> callChanges ) {
 
 			String lastCalledNumber = StringHelper.EmptyToNull( StringHelper.ExtractSubstring( page, "Last Called Number:<td><font color=\"darkblue\">", "<", lineState.Name + " Status" ) );
 			if( lastCalledNumber != lineState.LastCalledNumber )
-				lineChanges.Add( new LineChange( lineState, PROPERTY_LINE_LASTCALLEDNUMBER, lineState.LastCalledNumber, lastCalledNumber ) );
+				lineChanges.Add( new LinePropertyChange( lineState, PROPERTY_LINE_LASTCALLEDNUMBER, lineState.LastCalledNumber, lastCalledNumber ) );
 			lineState.LastCalledNumber = lastCalledNumber;
 				
 			String lastCallerNumber = StringHelper.EmptyToNull( StringHelper.ExtractSubstring( page, "Last Caller Number:<td><font color=\"darkblue\">", "<", lineState.Name + " Status" ) );
 			if( lastCallerNumber != lineState.LastCallerNumber )
-				lineChanges.Add( new LineChange( lineState, PROPERTY_LINE_LASTCALLERNUMBER, lineState.LastCallerNumber, lastCallerNumber ) );
+				lineChanges.Add( new LinePropertyChange( lineState, PROPERTY_LINE_LASTCALLERNUMBER, lineState.LastCallerNumber, lastCallerNumber ) );
 			lineState.LastCallerNumber = lastCallerNumber;
 
 			RegistrationState registrationState = GetRegistrationState( StringHelper.ExtractSubstring( page, "Registration State:<td><font color=\"darkblue\">", "<", lineState.Name + " Status" ) );
 			if( registrationState != lineState.RegistrationState )
-				lineChanges.Add( new LineChange( lineState, PROPERTY_LINE_REGISTRATIONSTATE, lineState.RegistrationState.ToString(), registrationState.ToString() ) );
+				lineChanges.Add( new LinePropertyChange( lineState, PROPERTY_LINE_REGISTRATIONSTATE, lineState.RegistrationState.ToString(), registrationState.ToString() ) );
 			lineState.RegistrationState = registrationState;
 
 			bool messageWaiting = StringHelper.ExtractSubstring( page, "Message Waiting:<td><font color=\"darkblue\">", "<", lineState.Name + " Status" ) != "No";
 			if( messageWaiting != lineState.MessageWaiting )
-				lineChanges.Add( new LineChange( lineState, PROPERTY_LINE_MESSAGEWAITING, lineState.MessageWaiting.ToString(), messageWaiting.ToString() ) );
+				lineChanges.Add( new LinePropertyChange( lineState, PROPERTY_LINE_MESSAGEWAITING, lineState.MessageWaiting.ToString(), messageWaiting.ToString() ) );
 			lineState.MessageWaiting = messageWaiting;
 
 			// Analyse both calls for the line. (We know this device only
@@ -102,70 +102,70 @@ namespace LothianProductions.VoIP.Monitor.Impl {
 			AnalyseCallState( page, lineState.Calls[ 1 ], lineState, callChanges );
         }
         
-        protected virtual void AnalyseCallState( String page, Call callState, Line lineState, IList<CallChange> callChanges ) {
+        protected virtual void AnalyseCallState( String page, Call callState, Line lineState, IList<CallPropertyChange> callChanges ) {
 			Activity callActivity = GetActivity( StringHelper.ExtractSubstring( page, callState.Name + " State:<td><font color=\"darkblue\">", "<", lineState.Name + " Status" ) );
 			if( callActivity != callState.Activity )
-				callChanges.Add( new CallChange( callState, PROPERTY_CALL_ACTIVITY, callState.Activity.ToString(), callActivity.ToString() ) );
+				callChanges.Add( new CallPropertyChange( callState, PROPERTY_CALL_ACTIVITY, callState.Activity.ToString(), callActivity.ToString() ) );
 			callState.Activity = callActivity;
 			
 			String duration = StringHelper.EmptyToNull( StringHelper.ExtractSubstring( page, callState.Name + " Duration:<td><font color=\"darkblue\">", "<", lineState.Name + " Status" ) );
 			if( duration != callState.Duration )
-				callChanges.Add( new CallChange( callState, PROPERTY_CALL_DURATION, callState.Duration, duration ) );
+				callChanges.Add( new CallPropertyChange( callState, PROPERTY_CALL_DURATION, callState.Duration, duration ) );
 			callState.Duration = duration;
 
 			CallType type = GetCallType( StringHelper.ExtractSubstring( page, callState.Name + " Type:<td><font color=\"darkblue\">", "<", lineState.Name + " Status" ) );
 			if( type != callState.Type )
-				callChanges.Add( new CallChange( callState, PROPERTY_CALL_TYPE, callState.Type.ToString(), type.ToString() ) );
+				callChanges.Add( new CallPropertyChange( callState, PROPERTY_CALL_TYPE, callState.Type.ToString(), type.ToString() ) );
 			callState.Type = type;
 
 			Tone tone = GetTone( StringHelper.ExtractSubstring( page, callState.Name + " Tone:<td><font color=\"darkblue\">", "<", lineState.Name + " Status" ) );
 			if( tone != callState.Tone )
-				callChanges.Add( new CallChange( callState, PROPERTY_CALL_TONE, callState.Tone.ToString(), tone.ToString() ) );
+				callChanges.Add( new CallPropertyChange( callState, PROPERTY_CALL_TONE, callState.Tone.ToString(), tone.ToString() ) );
 			callState.Tone = tone;
 			
 			String encoder = StringHelper.EmptyToNull( StringHelper.ExtractSubstring( page, callState.Name + " Encoder:<td><font color=\"darkblue\">", "<", lineState.Name + " Status" ) );
 			if( encoder != callState.Encoder )
-				callChanges.Add( new CallChange( callState, PROPERTY_CALL_ENCODER, callState.Encoder, encoder ) );
+				callChanges.Add( new CallPropertyChange( callState, PROPERTY_CALL_ENCODER, callState.Encoder, encoder ) );
 			callState.Encoder = encoder;
 			
 			String decoder = StringHelper.EmptyToNull( StringHelper.ExtractSubstring( page, callState.Name + " Decoder:<td><font color=\"darkblue\">", "<", lineState.Name + " Status" ) );
 			if( decoder != callState.Decoder )
-				callChanges.Add( new CallChange( callState, PROPERTY_CALL_DECODER, callState.Decoder, decoder ) );
+				callChanges.Add( new CallPropertyChange( callState, PROPERTY_CALL_DECODER, callState.Decoder, decoder ) );
 			callState.Decoder = decoder;
 			
 			long bytesSent = ParseInt64( StringHelper.ExtractSubstring( page, callState.Name + " Bytes Sent:<td><font color=\"darkblue\">", "<", lineState.Name + " Status" ) );
 			if( bytesSent != callState.BytesSent )
-				callChanges.Add( new CallChange( callState, PROPERTY_CALL_BYTESSENT, callState.BytesSent, bytesSent ) );
+				callChanges.Add( new CallPropertyChange( callState, PROPERTY_CALL_BYTESSENT, callState.BytesSent, bytesSent ) );
 			callState.BytesSent = bytesSent;
 
 			long bytesReceived = ParseInt64( StringHelper.ExtractSubstring( page, callState.Name + " Bytes Recv:<td><font color=\"darkblue\">", "<", lineState.Name + " Status" ) );
 			if( bytesReceived != callState.BytesReceived )
-				callChanges.Add( new CallChange( callState, PROPERTY_CALL_BYTESRECEIVED, callState.BytesReceived, bytesReceived ) );
+				callChanges.Add( new CallPropertyChange( callState, PROPERTY_CALL_BYTESRECEIVED, callState.BytesReceived, bytesReceived ) );
 			callState.BytesReceived = bytesReceived;
 			
 			long packetLoss = ParseInt64( StringHelper.ExtractSubstring( page, callState.Name + " Packets Lost:<td><font color=\"darkblue\">", "<", lineState.Name + " Status" ) );
 			if( packetLoss != callState.PacketLoss )
-				callChanges.Add( new CallChange( callState, PROPERTY_CALL_PACKETLOSS, callState.PacketLoss, packetLoss ) );
+				callChanges.Add( new CallPropertyChange( callState, PROPERTY_CALL_PACKETLOSS, callState.PacketLoss, packetLoss ) );
 			callState.PacketLoss = packetLoss;
 			
 			long packetError = ParseInt64( StringHelper.ExtractSubstring( page, callState.Name + " Packet Error:<td><font color=\"darkblue\">", "<", lineState.Name + " Status" ) );
 			if( packetError != callState.PacketError )
-				callChanges.Add( new CallChange( callState, PROPERTY_CALL_PACKETERROR, callState.PacketError, packetError ) );
+				callChanges.Add( new CallPropertyChange( callState, PROPERTY_CALL_PACKETERROR, callState.PacketError, packetError ) );
 			callState.PacketError = packetError;
 			
 			long jitter = ParseInt64( StringHelper.ExtractSubstring( page, callState.Name + " Jitter:<td><font color=\"darkblue\">", "<", lineState.Name + " Status" ).Replace( " ms", "") );
 			if( jitter != callState.Jitter )
-				callChanges.Add( new CallChange( callState, PROPERTY_CALL_JITTER, callState.Jitter, jitter ) );
+				callChanges.Add( new CallPropertyChange( callState, PROPERTY_CALL_JITTER, callState.Jitter, jitter ) );
 			callState.Jitter = jitter;
 			
 			long decodeLatency = ParseInt64( StringHelper.ExtractSubstring( page, callState.Name + " Decode Latency:<td><font color=\"darkblue\">", "<", lineState.Name + " Status" ).Replace( " ms", "") );
 			if( decodeLatency != callState.DecodeLatency )
-				callChanges.Add( new CallChange( callState, PROPERTY_CALL_DECODELATENCY, callState.DecodeLatency, decodeLatency ) );
+				callChanges.Add( new CallPropertyChange( callState, PROPERTY_CALL_DECODELATENCY, callState.DecodeLatency, decodeLatency ) );
 			callState.DecodeLatency = decodeLatency;
 			
 			long roundTripDelay = ParseInt64( StringHelper.ExtractSubstring( page, callState.Name + " Round Trip Delay:<td><font color=\"darkblue\">", "<", lineState.Name + " Status" ).Replace( " ms", "") );
 			if( roundTripDelay != callState.RoundTripDelay )
-				callChanges.Add( new CallChange( callState, PROPERTY_CALL_ROUNDTRIPDELAY, callState.RoundTripDelay, roundTripDelay ) );
+				callChanges.Add( new CallPropertyChange( callState, PROPERTY_CALL_ROUNDTRIPDELAY, callState.RoundTripDelay, roundTripDelay ) );
 			callState.RoundTripDelay = roundTripDelay;
         }
         
@@ -183,68 +183,49 @@ namespace LothianProductions.VoIP.Monitor.Impl {
         }
 
 		public virtual CallType GetCallType( String type ) {
-			if( type == "Inbound" )
-				return CallType.Inbound;
-			else if( type == "Outbound" )
-				return CallType.Outbound;
-			else if( type == "" )
-				return CallType.IdleDisconnected;
-			throw new ArgumentOutOfRangeException( "Type " + type + " not found." );
+			String mapping = ConfigurationManager.AppSettings[ this.GetType().Name + ":CallType:" + type ];
+			
+			if( mapping != null )
+				try {
+					return (CallType) Enum.Parse( typeof(CallType), mapping );
+				} catch (ArgumentException) {
+				}
+				
+			throw new DeviceConfigurationException( "CallType \"" + type + "\" incorrectly mapped to \"" + mapping + "\". Perhaps a configuration entry is missing?" );			
 		}
 		
 		public virtual Tone GetTone( String tone ) {
-			if( tone == "None" )
-				return Tone.None;
-			else if( tone == "Dial" )
-				return Tone.Dial;
-			else if( tone == "Secure Call Indication" )
-				return Tone.SecureCall;
-			else if( tone == "Busy" )
-				return Tone.Busy;
-			else if( tone == "Ring" )
-				return Tone.Call;
-			else if ( tone == "Ring Back" )
-			    return Tone.Error;
-			    
-			// FIXME are the following states accurately described?
-			else if (tone == "SIT 1")
-				return Tone.InvalidNumber;
-			else if (tone == "Reorder")
-			    return Tone.Error;
-			else if (tone == "Off Hook Warning")
-			    return Tone.Error;
-			    
-			// Note that an outgoing ringing call returns a tone of "none"
-			// so the call tone relates to an incoming call.
-			throw new ArgumentOutOfRangeException("Tone " + tone + " not found.");
+			String mapping = ConfigurationManager.AppSettings[ this.GetType().Name + ":Tone:" + tone ];
+
+			if( mapping != null )
+				try {
+					return (Tone) Enum.Parse( typeof(Tone), mapping );
+				} catch (ArgumentException) {
+				}
+			
+			throw new DeviceConfigurationException( "Tone \"" + tone + "\" incorrectly mapped to \"" + mapping + "\". Perhaps a configuration entry is missing?" );			
 		}
 
 		public virtual Activity GetActivity( String activity ) {
-			if( activity == "Idle" )
-				return Activity.IdleDisconnected;
-			else if( activity == "Connected" )
-				return Activity.Connected;
-			else if( activity == "Ringing" )
-				return Activity.Ringing;
-			else if( activity == "Dialing" )
-				return Activity.Dialing;
-			else if( activity == "Invalid" )
-				return Activity.Error;
-			else if (activity == "Calling")
-				return Activity.Ringing;
-			else if (activity == "Answering")
-				return Activity.Ringing;
-			else if (activity == "Proceeding")
-				return Activity.Ringing;
-			throw new ArgumentOutOfRangeException("Activity " + activity + " not found.");
+			String mapping = ConfigurationManager.AppSettings[ this.GetType().Name + ":Activity:" + activity ];
+			if( mapping != null )
+				try {
+					return (Activity) Enum.Parse( typeof(Activity), mapping );
+				} catch (ArgumentException) {
+				}
+			
+			throw new DeviceConfigurationException( "Activity \"" + activity + "\" incorrectly mapped to \"" + mapping + "\". Perhaps a configuration entry is missing?" );			
 		}
         
         public virtual RegistrationState GetRegistrationState( String state ) {
-			if( state == "Online" )
-				return RegistrationState.Online;
-			else if( state == "Offline" )
-				return RegistrationState.Offline;
-			throw new ArgumentOutOfRangeException( "State " + state + " not found." );
+			String mapping = ConfigurationManager.AppSettings[ this.GetType().Name + ":RegistrationState:" + state ];
+			if( mapping != null )
+				try {
+					return (RegistrationState) Enum.Parse( typeof(RegistrationState), mapping );
+				} catch (ArgumentException) {
+				}			
+			
+			throw new DeviceConfigurationException( "Registration state \"" + state + "\" incorrectly mapped to \"" + mapping + "\". Perhaps a configuration entry is missing?" );			
         }
     }
 }
