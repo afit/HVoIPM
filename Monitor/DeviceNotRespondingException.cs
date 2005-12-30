c37fe9a3302b6050fbf9f9b900aa40de1d00e262
Aidan Fitzpatrick <aidan@reincubate.com>
2005-12-30 12:08:49 +0000

    New exception type

--- Monitor/DeviceNotRespondingException.cs
+++ Monitor/DeviceNotRespondingException.cs
@@ -3,7 +3,33 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace LothianProductions.VoIP.Monitor {
-    public class DeviceNotRespondingException : ApplicationException {
+    public class DeviceException : ApplicationException {
+        public DeviceException( String message )
+            : base( message ) {
+        }
+
+        public DeviceException( String message, Exception e )
+            : base( message, e ) {
+        }
+    }
+
+    public class DeviceConfigurationException : DeviceException {
+        public DeviceConfigurationException( String message )
+            : base( message ) {
+        }
+
+        public DeviceConfigurationException( String message, Exception e )
+            : base( message, e ) {
+        }
+    }
+    
+    public class DeviceAccessUnauthorizedException : DeviceException {
+        public DeviceAccessUnauthorizedException( String message, Exception e )
+            : base( message, e ) {
+        }
+    }
+
+    public class DeviceNotRespondingException : DeviceException {
         public DeviceNotRespondingException( String message, Exception e )
             : base( message, e ) {
         }
