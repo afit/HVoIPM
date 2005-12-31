6622fe41849ebf4bfcbd939a6e56f88032c26357
Aidan Fitzpatrick <aidan@reincubate.com>
2005-12-31 17:31:08 +0000

    Greater robustness for DMC

--- Monitor/DeviceMonitorControl.cs
+++ Monitor/DeviceMonitorControl.cs
@@ -11,20 +11,47 @@ namespace LothianProductions.VoIP.Monitor {
 		}
 		
 		public void Run() {
-			try {
-				mDeviceMonitor.Run();
-			} catch (DeviceAccessUnauthorizedException e) {
-				MessageBox.Show( "The " + mDeviceMonitor.GetType().Name + " reported an authorisation error: " + e.Message + "\n\nMaybe the application's configuration is incorrect: consult \"Hardware VoIP Monitor.config\".\n\nHVoIPM will stop polling this monitor. If this failure is unexpected you should send your system.xml log file to hvoip@lothianproductions.co.uk for debugging.", "Device monitor error" );
-				Logger.Instance().Log( "The " + mDeviceMonitor.GetType().Name + " reported an authorisation error", e );
-			} catch (DeviceNotRespondingException e) {
-				MessageBox.Show( "The " + mDeviceMonitor.GetType().Name + " reported that its device failed to respond: " + e.Message + "\n\nMaybe the application's configuration is incorrect: consult \"Hardware VoIP Monitor.config\".\n\nHVoIPM will stop polling this monitor. If this failure is unexpected you should send your system.xml log file to hvoip@lothianproductions.co.uk for debugging.", "Device monitor error" );
-				Logger.Instance().Log( "The " + mDeviceMonitor.GetType().Name + " reported that its device failed to respond", e );
-			} catch (DeviceException e) {
-				MessageBox.Show( "The " + mDeviceMonitor.GetType().Name + " reported a general error: " + e.Message + "\n\nHVoIPM will stop polling this monitor. If this failure is unexpected you should send your system.xml log file to hvoip@lothianproductions.co.uk for debugging.", "Device monitor error" );
-				Logger.Instance().Log( "The " + mDeviceMonitor.GetType().Name + " reported a general error", e );
-			} catch (ApplicationException e) {
-				MessageBox.Show( "The " + mDeviceMonitor.GetType().Name + " reported a fatal error: " + e.Message + "\n\nHVoIPM will stop polling this monitor. If this failure is unexpected you should send your system.xml log file to hvoip@lothianproductions.co.uk for debugging.", "Device monitor error" );
-				Logger.Instance().Log( "The " + mDeviceMonitor.GetType().Name + " reported a fatal error", e );
+			// Wrap in a big while to prevent the run method returning.
+			while( true ) {
+				try {
+					mDeviceMonitor.Run();
+				} catch (DeviceAccessUnauthorizedException e) {
+					Logger.Instance().Log( "The " + mDeviceMonitor.GetType().Name + " reported an authorisation error", e );
+					if( MessageBox.Show(
+						"The " + mDeviceMonitor.GetType().Name + " reported an authorisation error: " + e.Message +
+						"\n\nMaybe the application's configuration is incorrect: consult \"Hardware VoIP Monitor.config\"." +
+						"\n\nIf this failure is unexpected you should check for updates before sending your system.xml log file to hvoip@lothianproductions.co.uk for debugging." +
+						"\n\nShould HVoIPM continue polling this device monitor?",
+						 "HVoIPM device monitor error", MessageBoxButtons.YesNo, MessageBoxIcon.Error ) == DialogResult.No )
+						return;					
+				} catch (DeviceNotRespondingException e) {
+					Logger.Instance().Log( "The " + mDeviceMonitor.GetType().Name + " reported that its device failed to respond", e );
+					if( MessageBox.Show(
+						"The " + mDeviceMonitor.GetType().Name + " reported that its device failed to respond: " + e.Message +
+						"\n\nMaybe the application's configuration is incorrect: consult \"Hardware VoIP Monitor.config\"." +
+						"\n\nIf this failure is unexpected you should check for updates before sending your system.xml log file to hvoip@lothianproductions.co.uk for debugging." +
+						"\n\nShould HVoIPM continue polling this device monitor?",
+						 "HVoIPM device monitor error", MessageBoxButtons.YesNo, MessageBoxIcon.Error ) == DialogResult.No )
+						return;
+					
+				} catch (DeviceException e) {
+					Logger.Instance().Log( "The " + mDeviceMonitor.GetType().Name + " reported a general error", e );
+					if( MessageBox.Show(
+						"The " + mDeviceMonitor.GetType().Name + " reported a general error: " + e.Message +
+						"\n\nIf this failure is unexpected you should check for updates before sending your system.xml log file to hvoip@lothianproductions.co.uk for debugging." +
+						"\n\nShould HVoIPM continue polling this device monitor?",
+						 "HVoIPM device monitor error", MessageBoxButtons.YesNo, MessageBoxIcon.Error ) == DialogResult.No )
+						return;
+				} catch (ApplicationException e) {
+					Logger.Instance().Log( "The " + mDeviceMonitor.GetType().Name + " reported a fatal error", e );
+					if( MessageBox.Show(
+						"The " + mDeviceMonitor.GetType().Name + " reported a fatal error: " + e.Message +
+						"\n\nIf this failure is unexpected you should check for updates before sending your system.xml log file to hvoip@lothianproductions.co.uk for debugging." +
+						"\n\nShould HVoIPM continue polling this device monitor?",
+						"HVoIPM device monitor error",
+						MessageBoxButtons.YesNo, MessageBoxIcon.Error ) == DialogResult.No )
+						return;
+				}
 			}
 		}
 		
